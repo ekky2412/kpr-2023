db5d01deb5166201a0b69e1a4441c5300f5db54d
rsstst <snepshot551@gmail.com>
2023-07-17 12:09:56 +0700

    moved code

--- menu.js
+++
@@ -1,9 +0,0 @@
-function onClickDetails(){
-    var img = document.getElementById('img')
-    
-    if (img.style.display == "block"){
-        img.style.display = "none";
-    } else {
-        img.style.display = "block";
-    }
-}
